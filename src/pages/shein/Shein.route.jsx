030f5870613dc2add19a37a48fbb034f6d2dbed2
Mohamed gamal <mohamed555asdfgh@gmail.com>
2024-07-15 22:25:37 +0300

    build/changes

--- src/pages/shein/Shein.route.jsx
+++ src/pages/shein/Shein.route.jsx
@@ -15,10 +15,10 @@ const Shein = () => {
           <div className="flex gap-5 flex-col md:flex-row">
             <div className="filter w-full">
               <div className="flex items-center justify-center md:justify-between flex-col md:flex-row mb-7">
-                <h3 className="text-[19px] text-gray-500 font-semibold">
+                <h3 className="text-[19px] text-gray-500 font-semibold" data-aos="zoom-in" data-aos-delay="200">
                   11 {t("results")}
                 </h3>
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-3" data-aos="zoom-in" data-aos-delay="200">
                   <span className="font-bold">{t("sort_by")}: </span>
                   <select className="border border-gray-300 p-1 rounded-md outline-none focus:border-primary">
                     <option value="">Recommended</option>
@@ -31,9 +31,9 @@ const Shein = () => {
 
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {data.length > 0 &&
-                  data.map((item) => {
+                  data.map((item , i) => {
                     return (
-                      <div className="box">
+                      <div className="box" data-aos={(i+1) % 2 == 0 ? "fade-left" : "fade-right"} data-aos-delay="300" >
                         <div className="img">
                           <img src={imgai} alt="" />
                           <div className="price">
--- src/pages/signAs/SignUpAsHostinger.jsx
+++ src/pages/signAs/SignUpAsHostinger.jsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { t } from "i18next";
-import logo from "../../assets/images/bird-2374419_1920.jpg";
+import img from "../../assets/images/bouquet-4736413_1920.jpg";
 import "./signAs.scss";
 import Banner from "../../components/banner/Banner.component";
 
@@ -10,27 +10,39 @@ const SignUpAsHostinger = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const boxRef = useRef();
+  useEffect(() => {
+    boxRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   return (
     <Fragment>
-      <Banner title={t("Start_With_Us")}/>
-      <div class="sign">
+      <Banner title={t("Start_With_Us")} />
+      <div className="sign" data-aos="zoom-in" data-aos-delay="200">
         <div className="all">
+          <div
+            className="img"
+            data-aos="flip-up"
+            data-aos-delay="600"
+            data-aos-easing="ease-out-cubic"
+          >
+            <img src={img} alt="" />
+          </div>
           <form action="#">
-            <div class="form">
-              <div class="details personal">
-                <span class="title">{t("Personal_Details")}</span>
-                <div class="fields">
-                  <div class="input-field">
+            <div className="form" data-aos="fade-left" data-aos-delay="700">
+              <div className="details personal">
+                <span className="title">{t("Personal_Details")}</span>
+                <div className="fields">
+                  <div className="input-field">
                     <label>{t("full Name")}</label>
                     <input type="text" placeholder="Enter your name" required />
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("address")}</label>
                     <input type="text" placeholder="Enter Adress" required />
                   </div>
 
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("email")}</label>
                     <input
                       type="text"
@@ -38,7 +50,7 @@ const SignUpAsHostinger = () => {
                       required
                     />
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("Password")}</label>
                     <input
                       type="text"
@@ -47,7 +59,7 @@ const SignUpAsHostinger = () => {
                     />
                   </div>
 
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("phone")}</label>
                     <input
                       type="number"
@@ -56,7 +68,7 @@ const SignUpAsHostinger = () => {
                     />
                   </div>
 
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("gender")}</label>
                     <select required>
                       <option disabled selected>
@@ -67,7 +79,7 @@ const SignUpAsHostinger = () => {
                       <option>Others</option>
                     </select>
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("Age")}</label>
                     <input
                       type="number"
@@ -78,10 +90,10 @@ const SignUpAsHostinger = () => {
                 </div>
               </div>
 
-              <div class="details prand">
-                <span class="title">{t("Prand_Details")}</span>
-                <div class="fields">
-                  <div class="input-field">
+              <div className="details prand">
+                <span className="title">{t("Prand_Details")}</span>
+                <div className="fields">
+                  <div className="input-field">
                     <label>{t("Prand_Name")}</label>
                     <input
                       type="text"
@@ -89,19 +101,20 @@ const SignUpAsHostinger = () => {
                       required
                     />
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("address")}</label>
                     <input type="text" placeholder="Enter adress" required />
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("Location")}</label>
                     <input type="text" placeholder="Enter locaton" required />
                   </div>
-                  <div class="input-field">
+                  <div className="input-field">
                     <label>{t("Place_Photos")}</label>
-                    <input type="file"  required />
+                    <input type="file" required />
                   </div>
-                  <div class="input-field">
+
+                  <div className="input-field">
                     <label>{t("Service Type")}</label>
                     <select required>
                       <option disabled selected>
@@ -111,51 +124,54 @@ const SignUpAsHostinger = () => {
                       <option>{t("Recive customers")}</option>
                     </select>
                   </div>
-                  <div class="input-field mega">
-                    <label>{t("Working Dates")}</label>
+                  <div className="input-field mega">
                     <div className="con">
                       <div className="inMega">
-                        <label htmlFor=""> {t("from (hours)")}</label>
+                        <label htmlFor=""> {t("Working From (hours)")}</label>
                         <input type="number" placeholder="hours" />
                       </div>
                       <div className="inMega">
-                        <label htmlFor=""> {t("to (hours)")}</label>
+                        <label htmlFor=""> {t("Working to (hours)")}</label>
                         <input type="number" placeholder="hours" />
                       </div>
-                      <div className="inMega holi">
-                        <label htmlFor="">{t("Holidays")}</label>
-                        <span>
-                          <input type="checkbox" /> {t("Saturday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Sunday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Monday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Tuesday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Wednesday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Thursday")}
-                        </span>
-                        <span>
-                          <input type="checkbox" /> {t("Friday")}
-                        </span>
-                      </div>
+                    </div>
+                  </div>
+                  <div className="holi">
+                    <label htmlFor="">{t("Holidays")}</label>
+                    <div className="days">
+                      <span>
+                        <input type="checkbox" /> {t("Saturday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Sunday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Monday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Tuesday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Wednesday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Thursday")}
+                      </span>
+                      <span>
+                        <input type="checkbox" /> {t("Friday")}
+                      </span>
                     </div>
                   </div>
                 </div>
               </div>
+
               <button type="submit">
-                <span class="btnText">{t("Enroll")}</span>
+                <span className="btnText">{t("Enroll")}</span>
               </button>
             </div>
           </form>
         </div>
+        <h1 ref={boxRef}></h1>
       </div>
       {loading && <Loader plainBackground />}
     </Fragment>
